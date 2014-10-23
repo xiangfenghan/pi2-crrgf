9b918eda92047099c0ea89369cf933512e84d1dd
Solineoz <eric.revelle@gmail.com>
2014-10-23 19:20:20 -0400

    Nettoyage

--- vues/VueTest.class.php
+++ vues/VueTest.class.php
@@ -2,11 +2,14 @@
 class VueTest{
 
 	public static function afficherTest() {
+
 		Vue::head('Titre','La description');
 		Vue::header();
 		Vue::nav();
 		$oTest = new Test();
 		echo $oTest->echoTest();
 		Vue::footer();
+
 	}
+
 }
