ca98a0b1b2468b8cc9f6204a7efdf7e8ded85cf5
Solineoz <eric.revelle@gmail.com>
2014-10-23 13:57:59 -0400

    Dossier modeles et Vue de teste

---
+++ vues/VueTest.class.php
@@ -0,0 +1,12 @@
+<?php 
+class VueTest{
+
+	public static function afficherTest() {
+		Vue::head('Titre','La description');
+		Vue::header();
+		Vue::nav();
+		$oTest = new Test();
+		echo $oTest->echoTest();
+		Vue::footer();
+	}
+}
