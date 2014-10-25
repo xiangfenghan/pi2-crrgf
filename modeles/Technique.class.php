8566f16ce2a1b1620c584c6df08ad48ebf64c0f1
Solineoz <eric.revelle@gmail.com>
2014-10-25 14:44:06 -0400

    Ajout des modeles

---
+++ modeles/Technique.class.php
@@ -0,0 +1,42 @@
+﻿<?php
+
+class Technique{
+
+	private $idTechnique;
+	private $sNomTechnique;
+
+public function __construct($idTechnique=0, $sNomTechnique=" ")
+{
+	$this->setIdTechnique($idTechnique);
+	$this->setNomTechnique($sNomTechnique);
+}
+
+/**************LES SET******************/
+	public function setIdTechnique($idTechnique)
+	{
+		TypeException::estNumerique($idTechnique);
+		$this->idTechnique = $idTechnique;
+	}//fin de la fonction setIdTechnique()
+
+
+	public function setNomTechnique($sNomTechnique)
+	{
+		TypeException::estVide($sNomTechnique);
+		TypeException::estString($sNomTechnique);
+		$this->sNomTechnique= $sNomTechnique;
+	}//fin de la fonction setNomTechnique()
+
+
+/**************LES GET******************/
+	public function getIdTechnique()
+	{
+		return $this->idTechnique;
+	}//fin de la fonction getIdTechnique()
+
+	public function getNomTechnique()
+	{
+		return htmlentities($this->sNomTechnique);
+	}//fin de la fonction getNomTechnique()
+
+
+}
